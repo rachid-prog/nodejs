3344dad0053cb266d5482c2100e18cd83a1dd9d0
rachid <12irachid@gmail.com>
2025-03-16 15:01:33 +0100

    Modification db.cionfig.js

--- src/database/db.config.js
+++ src/database/db.config.js
@@ -11,9 +11,9 @@ if (process.env.NODE_ENV === 'production') {
 
 }
 else {
-    db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.PASSWORD, {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+    db = new Sequelize("pokedex", "root", "", {
+        host: "localhost",
+        dialect: "mariadb",
         logging: false,
     })
 
